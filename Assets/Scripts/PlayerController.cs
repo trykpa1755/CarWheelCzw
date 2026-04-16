6a643699e3f3af57573a4cd11d80a805daed95ee
trykpa1755 <trykpa1755@wp.pl>
2026-04-16 20:10:09 +0200

    Lekcja39

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
         float accel = Input.GetAxis("Vertical");
         float steer = Input.GetAxis("Horizontal");
         float brake = Input.GetAxis("Jump");
+        if (!RaceController.racing) accel = 0;
         drivingScript.Drive(accel, brake, steer);
         drivingScript.EngineSound();
     }
