40f5e05eb4e0714041c204cd1bf3c2a134cf232e
trykpa1755 <trykpa1755@wp.pl>
2026-04-16 19:03:00 +0200

    Initial commit

---
+++ Assets/Scripts/DrivingScript.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivingScript : MonoBehaviour
+{
+    public WheelScript[] wheels; // tutaj beda wszystkie nasze kola
+    public float torque = 200; // moment obrotowy
+    public float maxSteerAngle = 30; // maksymalny k¹t wychylenia
+    public float maxBrakeTorque = 500; // moment hamowania
+    public float maxSpeed = 150; // max prêdkoœæ
+    public Rigidbody rb;
+    public float currentSpeed; // aktualna predkosc
+
+    public AudioSource engineSound;
+    float rpm;
+    public int currentGear = 1;
+    public float currentGearPerc;
+    public int numGears = 5;
+    public float gearLength = 5f;
+
+
+    public GameObject backLights;
+    // Start is called before the first frame update
+
+    public void Drive(float accel, float brake, float steer)
+    {
+        accel = Mathf.Clamp(accel, -1, 1);
+        steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
+        brake = Mathf.Clamp(brake, 0, 1) * maxBrakeTorque;
+
+
+        if(brake != 0)
+        {
+            backLights.SetActive(true);
+        }
+        else
+        {
+            backLights.SetActive(false);
+        }
+
+
+        float thrustTorque = 0;
+        currentSpeed = rb.velocity.magnitude * 3.6f;
+        if (currentSpeed < maxSpeed)
+            thrustTorque = accel * torque;
+
+        foreach (WheelScript wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = thrustTorque;
+
+            if (wheel.frontWheel)
+                wheel.wheelCollider.steerAngle = steer;
+
+            else
+                wheel.wheelCollider.brakeTorque = brake;
+
+            Quaternion quat;
+            Vector3 position;
+            wheel.wheelCollider.GetWorldPose(out position, out quat);
+            wheel.wheel.transform.position = position;
+            wheel.wheel.transform.rotation = quat;
+        }
+    }
+
+    public void EngineSound()
+    {
+        float gearPercentage = (1 / (float)numGears);
+        float targetGearFactor = Mathf.InverseLerp(gearPercentage * currentGear, gearPercentage *
+        (currentGear + 1), Mathf.Abs(currentSpeed / maxSpeed));
+        //okreœlamy ile mocy naszego biegu mamy, jak dojdzie do 100 lub 0 bêdziemy zmieniaæ bieg
+        currentGearPerc = Mathf.Lerp(currentGearPerc, targetGearFactor, Time.deltaTime * 5f);
+        // wyliczamy obroty silnika oraz na jakiej prêdkoœci zmieniamy bieg
+        var gearNumFactor = currentGear / (float)numGears;
+        rpm = Mathf.Lerp(gearNumFactor, 1, currentGearPerc);
+        float speedPercentage = Mathf.Abs(currentSpeed / maxSpeed);
+        float upperGearMax = (1 / (float)numGears) * (currentGear + 1);
+        float downGearMax = (1 / (float)numGears) * currentGear;
+        //zmiana biegów
+        if (currentGear > 0 && speedPercentage < downGearMax) currentGear--;
+        if (speedPercentage > upperGearMax && (currentGear < (numGears - 1))) currentGear++;
+        //zmiana dŸwiêku
+        float pitch = Mathf.Lerp(1, 6, rpm);
+        engineSound.pitch = Mathf.Min(6, pitch) * 0.15f;
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    DrivingScript drivingScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        drivingScript = GetComponent<DrivingScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float accel = Input.GetAxis("Vertical");
+        float steer = Input.GetAxis("Horizontal");
+        float brake = Input.GetAxis("Jump");
+        drivingScript.Drive(accel, brake, steer);
+        drivingScript.EngineSound();
+    }
+}
